28f4a385dd5f8647048fd5df896f3a467fc84c40
minji7901 <kimem7901@gmail.com>
2024-11-12 12:30:39 +0900

    styled

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledNotFoundPage = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  height: 100vh;
+  background: #8bc34a;
+  h1 {
+    margin-bottom: 20px;
+    font-size: 4rem;
+  }
+  a {
+    display: block;
+    text-align: center;
+    padding: 10px 0;
+    font-size: 1.5rem;
+    border-radius: 5px;
+    background: #f06292;
+  }
+`;
 
 export default function NotFound() {
   return (
-    <div>
-      notFound
-    </div>
+    <StyledNotFoundPage>
+      <div>
+        <h1>Not Found</h1>
+        <Link to="/">go to Home</Link>
+      </div>
+    </StyledNotFoundPage>
   );
 }
-
